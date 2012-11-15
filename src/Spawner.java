911b5c1284235a7782fd650968c0902c1266a004
Luke Parker <lukepipod@gmail.com>
2012-11-15 06:38:07 +0000

    Removed Mob Spawned Message printed to console

--- src/Spawner.java
+++ src/Spawner.java
@@ -15,7 +15,6 @@ public class Spawner implements Runnable {
 		while(isRunning){
 			if(Component.mob.toArray().length < Tile.maxMobs && Sky.time == Sky.night){
 					spawnMob(new Red(new Random().nextInt((Level.worldW - 2) * Tile.tileSize) + Tile.tileSize + 30,50, Tile.tileSize, Tile.tileSize *2, Tile.mobRed));
-					System.out.println("Mob Spawned");
 				}
 			try{
 				Thread.sleep(new Random().nextInt(8000) + 5000);
